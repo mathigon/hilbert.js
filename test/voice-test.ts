8c68d3df1bc45843f770b3357c74fa95259c189b
Philipp Legner <philipp@mathigon.org>
2020-06-01 09:52:54 +0200

    Fix tests

--- test/voice-test.ts
+++ test/voice-test.ts
@@ -14,7 +14,7 @@ const voice = (src: string) => Expression.parse(src).toVoice();
 tape('Basic Voice', (test) => {
   test.equal(voice('sqrt(5)'), 'square root of 5');
   test.equal(voice('a * b'), 'a times b');
-  test.equal(voice('(a * b)'), 'open bracket a times b close bracket');
+  test.equal(voice('(a * b)'), 'a times b');
   test.equal(voice('a^b'), 'a to the power of b');
   test.equal(voice('f(x)'), 'f of x');
   test.equal(voice('a/b'), 'a over b');
