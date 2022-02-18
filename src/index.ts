afa41e2f77c53eaf297d87712567243f35601b31
Philipp Legner <philipp@mathigon.org>
2022-02-18 01:38:53 +0000

    Export additional functions

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export {ExprError} from './errors';
 export {Expression} from './expression';
 export {ExprElement, ExprIdentifier, ExprNumber, ExprOperator} from './elements';
 export {ExprFunction} from './functions';
-export {CONSTANTS as HILBERT_CONSTANTS} from './symbols';
+export {CONSTANTS as HILBERT_CONSTANTS, SPECIAL_IDENTIFIERS, isSpecialFunction} from './symbols';
+export {hasZero, Interval, isWhole, width} from './eval';
